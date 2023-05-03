190f1c39cec03bac25d9de041c09e8df726bf357
Caio Cezar Oliveira Filardi do Carmo <37853343+cezac@users.noreply.github.com>
2023-05-03 15:10:52 -0300

    Update buscaseq.c

--- Lab6_Parte3/buscaseq.c
+++ Lab6_Parte3/buscaseq.c
@@ -1,8 +1,12 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "time.h"
+clock_t comeco;
+clock_t final;
 
 int* guarda_Vetor (FILE *arquivo, int tamanho) // Função para armazenar os elementos do txt dentro do vetor
 {
+    comeco = clock();
     int *vetor = malloc(sizeof(int) * tamanho);
     int aux;
     int interator = 0;
@@ -17,11 +21,14 @@ int* guarda_Vetor (FILE *arquivo, int tamanho) // Função para armazenar os ele
             interator++;
         }
     }
+    final = clock();
+    printf ("Tempo de processamento para armazenar o vetor = %f\n", ((float) comeco - final) / ((CLOCKS_PER_SEC)));
     return vetor;
 }
 
 void ordena_Vetor(int *vetor, int tamanho) // Função que ira ordenar o vetor
 {
+    comeco = clock();
     int aux;
     for (int i=0; i < tamanho-1; i++)
     {
@@ -35,10 +42,13 @@ void ordena_Vetor(int *vetor, int tamanho) // Função que ira ordenar o vetor
             }
         }
     }
+    final = clock();
+    printf ("Tempo de processamento para ordenar o vetor = %f\n", ((float) comeco - final) / ((CLOCKS_PER_SEC)));
 }
 
 int buscaBinaria(int *vetor, int elemento, int tamanho) //Defina uma função de busca binária que recebe dois parâmetros: um vetor de inteiros e o elemento que se deseja encontrar
 {
+    comeco = clock();
     //Declare variáveis inteiras min e max
     int min = 0;
     int max = tamanho - 1;
@@ -51,6 +61,7 @@ int buscaBinaria(int *vetor, int elemento, int tamanho) //Defina uma função de
         //Se o elemento no meio do vetor for igual ao elemento procurado
         if (vetor[meio] == elemento) {
             //Retorne a posição do elemento
+            printf ("Tempo de processamento para encontrar o elemento = %f\n", ((float) comeco - final) / ((CLOCKS_PER_SEC)));
             return meio;
         }
             //Se o elemento no meio do vetor for menor que o elemento procurado
@@ -62,6 +73,8 @@ int buscaBinaria(int *vetor, int elemento, int tamanho) //Defina uma função de
             max = meio - 1;
         }
     }
+    final = clock();
+    printf ("Tempo de processamento para encontrar o elemento = %f\n", ((float) comeco - final) / ((CLOCKS_PER_SEC)));
     return -1;
 }
 
